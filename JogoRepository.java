71583b6719881a98855a1074e259f839e6753cad
OFFLKS <lukitug@gmail.com>
2026-06-23 07:29:45 -0300

    limpa estrutura incorreta

--- JogoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.locadora.repositories;
-
-import com.locadora.models.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long> {
-}
